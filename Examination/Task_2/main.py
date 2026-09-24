907c686ae0132699c743c484ec8017cfe0a680a5
mastercheecks25-ai <mastercheecks25@gmail.com>
2026-09-24 18:27:45 +0200

    changes made

--- Examination/Task_2/main.py
+++ Examination/Task_2/main.py
@@ -1,16 +1,23 @@
 shopping_list = []
 title = input("Enter the title: ")
+items = 0
 
 while True:
-    choice = int(input("\n1. Add an item\n2. Print currently added items\n3. Finished\n\nEnter your choice: "))
+    choice = input("\n1. Add an item\n2. Print currently added items\n3. Finished\n\nEnter your choice: ")
 
     if choice == 1:
         item = input("Enter an item: ")
         shopping_list.append(item)
+        item += 1
+
     elif choice == 2:
         print("Currently added items:")
         for i in shopping_list:
             print(f'{i}')
+
+    elif choice == "S":
+        print (f"Currently the list contains {items} item(s)")
+
     elif choice == 3:
         print("-" * 20)
         print(f"{title:^20}")
