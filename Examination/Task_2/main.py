0659719f2ed11e567e38364c837dec46a9edf99e
mastercheecks25-ai <mastercheecks25@gmail.com>
2026-09-24 19:25:36 +0200

    task 2 completed

--- Examination/Task_2/main.py
+++ Examination/Task_2/main.py
@@ -3,22 +3,32 @@ title = input("Enter the title: ")
 items = 0
 
 while True:
-    choice = input("\n1. Add an item\n2. Print currently added items\n3. Finished\n\nEnter your choice: ")
+    choice = str(input("\n1. Add an item\n2. Print currently added items\n3. Finished\n\nEnter your choice: "))
 
-    if choice == 1:
-        item = input("Enter an item: ")
-        shopping_list.append(item)
-        item += 1
+    if choice == "1":
+        #--------------------------------------------grade 4--------------------------------------#
+        if items > 9:
+            print("ERROR: The list is full, your item was not added")
+        #-------------------------------------------------------------------------------------------#
+        else:
+            item = input("Enter an item: ")
+            shopping_list.append(item)
+            items += 1
 
-    elif choice == 2:
+    elif choice == "2":
         print("Currently added items:")
         for i in shopping_list:
             print(f'{i}')
-
+        
+    #--------------------------------------------grade 3------------------------------------------------#
     elif choice == "S":
         print (f"Currently the list contains {items} item(s)")
+        #------------------------------------------grade 5-----------------------#
+        print(f'[{("*"*items):<10}]')
+        #------------------------------------------------------------------------#
+    #---------------------------------------------------------------------------------------------------#
 
-    elif choice == 3:
+    elif choice == "3":
         print("-" * 20)
         print(f"{title:^20}")
         print("-" * 20)
